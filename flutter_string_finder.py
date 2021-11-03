ae8f190e62cf83366a2909998873424b5a12f8df
DanieleF98 <DanieleF98@github.com>
2021-11-04 00:42:30 +0100

    now it works with the right format in console ready to be put inside a common string file

--- flutter_string_finder.py
+++ flutter_string_finder.py
@@ -1,14 +1,29 @@
 import os
 import os.path
 import re
+from enum import Enum
+
+class DirectoriesToSkip(Enum):
+    models = 1
+
 for dirpath, dirnames, filenames in os.walk(os.path.dirname(__file__)):
-    for filename in [f for f in filenames if f.endswith(".dart")]:
-            try:
+    for dirnameg in dirnames:
+        for dir in DirectoriesToSkip:
+            if(dirnameg.__contains__(f'{dir.name}')):
+                continue
+        else:
+            for filename in [f for f in filenames if f.endswith(".dart")]:
                 with open(os.path.join(dirpath, filename), "r") as f:
                     for line in f:
-                        if (line.__contains__('\"') | line.__contains__('\'')) & line.__contains__('package') == False:
-                            strTag = re.search("\"(.+?)\"|\'(.+?)\'", line)
-                            if(strTag):
-                                print("{}".format(strTag.group(0)))
-            except:
-                continue
+                        if(re.search('\$', line) == None):
+                            if (line.__contains__('dart') == False & line.__contains__('package') == False):
+                                strTag = re.search("\"(.+?)\"|\'(.+?)\'", line)
+                                if(strTag):
+                                    print("const {} = {};".format(re.sub(
+                                        '[^A-Za-z0-9]+', '', strTag.group(0).title()), strTag.group(0).replace('\'', "\\'")))
+                                else:
+                                    continue
+                            else:
+                                continue
+                        else:
+                            continue
